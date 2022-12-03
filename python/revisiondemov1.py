28fd42e45b7b9ccd2caeffb097ec5e510a44d9ef
juchani <victorfrancojuchani@gmail.com>
2022-12-03 12:27:09 -0400

    actualizacion

--- python/revisiondemov1.py
+++ python/revisiondemov1.py
@@ -6,19 +6,20 @@ from time import sleep as t
 
 cliente = influx(database='marcodb')
 lim_m=6
-lim_t=18
-
+lim_t=19
+global va
+va=True
 lat=-31.41733910
 lon=-64.183319
-gtm=-4
+gtm=-3
 luz=0
 ip1="192.168.0.66"
 def monitoreo():
     ####################PANEL 4 (FIJO)
-    a=ModbusClient(host="192.168.0.66", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
+    a=ModbusClient(host="192.168.25.66", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
     regs = a.read_holding_registers(109, 4)
     if(regs is not None):
-        if(regs[1]is not 0 and regs[0]is not 0):
+        if(regs[1]is not 0 or regs[2]is not 0):
             data = []
             data.append("CORRIENTE,tag=g{} val={}".format(4,regs[0]/1000))
             cliente.write_points(data, database='', time_precision='s', protocol='line')
@@ -30,9 +31,21 @@ def monitoreo():
             data.append("POTENCIA,tag=g{} val={}".format(4,potencia))
             cliente.write_points(data, database='', time_precision='s', protocol='line')
             
+            data = []
+            data.append("CORRIENTE,tag=s{} val={}".format(4,regs[2]/1000))
+            cliente.write_points(data, database='', time_precision='s', protocol='line')
+            data = []
+            data.append("VOLTAJE,tag=s{} val={}".format(4,regs[3]/10))
+            cliente.write_points(data, database='', time_precision='s', protocol='line')
+            potencia=((int(regs[3])/10)*(int(regs[2])/1000))
+            print(potencia)
+            data = []
+            data.append("POTENCIA,tag=s{} val={}".format(4,potencia))
+            cliente.write_points(data, database='', time_precision='s', protocol='line')
+            
             
     ####################PANEL 2 (EJE Y)
-    b=ModbusClient(host="192.168.0.73", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
+    b=ModbusClient(host="192.168.23.73", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
     regs1 = b.read_holding_registers(109, 4)
     if(regs1 is not None):
         if(regs1[1]is not 0 and regs1[0]is not 0):
@@ -59,7 +72,7 @@ def monitoreo():
             data.append("POTENCIA,tag=g{} val={}".format(2,potencia))
             cliente.write_points(data, database='', time_precision='s', protocol='line')
      ####################PANEL 3 (EJE X)
-    c=ModbusClient(host="192.168.0.23", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
+    c=ModbusClient(host="192.168.23.23", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
     regs2 = c.read_holding_registers(109, 4)
     if(regs2 is not None):
         if(regs2[1]is not 0 and regs2[0]is not 0):
@@ -88,10 +101,10 @@ def monitoreo():
             cliente.write_points(data, database='', time_precision='s', protocol='line')
             
      ####################PANEL 1 (EJE X-Y)
-    d=ModbusClient(host="192.168.0.3", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
+    d=ModbusClient(host="192.168.23.3", port=502, unit_id=1, auto_open=True)################PANEL FIJO#####################  
     regs3 = d.read_holding_registers(109, 4)
     if(regs3 is not None):
-        if(regs3[1]is not 0 and regs3[0]is not 0):
+        if(regs3[1]is not 0 or regs3[0]is not 0):
             print(regs3)
             data = []
             data.append("CORRIENTE,tag=s{} val={}".format(1,regs3[0]/1000))
@@ -172,12 +185,14 @@ def posicion_sol(i,mi):
         print("Hora={}:{} H={}  L={}".format(i,mi,h,l))
          ####################PANEL 1 (EJE X-Y)
         a = ModbusClient(host="192.168.0.3", port=502, unit_id=1, auto_open=True)
+        #
+        a.write_single_register(101, int(l))
+        t(5)
         a.write_single_register(107, int(h))
-        a.write_single_register(101, int(l))   
         ####################PANEL 2 (EJE Y)
         b = ModbusClient(host="192.168.0.73", port=502, unit_id=1, auto_open=True)
         b.write_single_register(107, int(h))          
-        ####################PANEL 3 (EJE z)
+        ####################PANEL 3 (EJE X)
         c = ModbusClient(host="192.168.0.23", port=502, unit_id=1, auto_open=True)
         c.write_single_register(101, int(l))          
                  
@@ -192,6 +207,29 @@ def posicion_sol(i,mi):
             
 
     print("Hora={}:{}".format(i,mi))
+def pos_ini(h,m):
+    global va
+    if(h>lim_m and h<lim_t):
+        va=True
+        return True
+    else:
+        if(va==True):  
+            t(5)
+            a = ModbusClient(host="192.168.0.3", port=502, unit_id=1, auto_open=True)
+            a.write_single_register(113, 100)
+            t(5)
+            b = ModbusClient(host="192.168.0.73", port=502, unit_id=1, auto_open=True)
+            b.write_single_register(107, 30)
+            t(5)
+            c = ModbusClient(host="192.168.0.23", port=502, unit_id=1, auto_open=True)
+            c.write_single_register(113, 100)          
+            t(5)
+            de = ModbusClient(host="192.168.0.3", port=502, unit_id=1, auto_open=True)
+            de.write_single_register(107, 30)
+            t(5)
+        return False
+        
+    
 while 1:
     try:
         dt = datetime.datetime.now()
@@ -199,8 +237,8 @@ while 1:
         hora=int(dt.strftime("%H"))
         minuto=int(dt.strftime("%M"))
         monitoreo()
-##        posicion_sol(hora,minuto)
-        posicion_sol(hora,minuto)
+        if(pos_ini(hora,minuto)):
+            posicion_sol(hora,minuto)
         t(0.5)
     except:
         print("error de conexión")
